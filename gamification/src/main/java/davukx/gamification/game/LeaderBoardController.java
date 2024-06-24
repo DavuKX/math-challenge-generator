b5f3d8a47225531324169980402b0d3de488406b
DavuKX <duvandavid2001@gmail.com>
2024-06-24 10:08:01 -0500

    set up load balancer

--- gamification/src/main/java/davukx/gamification/game/LeaderBoardController.java
+++ gamification/src/main/java/davukx/gamification/game/LeaderBoardController.java
@@ -2,6 +2,7 @@ package davukx.gamification.game;
 
 import lombok.RequiredArgsConstructor;
 import davukx.gamification.game.domain.LeaderBoardRow;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 /**
  * This class implements a REST API for the Gamification LeaderBoard service.
  */
+@Slf4j
 @RestController
 @RequestMapping("/leaders")
 @RequiredArgsConstructor
@@ -20,6 +22,7 @@ class LeaderBoardController {
 
     @GetMapping
     public List<LeaderBoardRow> getLeaderBoard() {
+        log.info("Retrieving leaderboard");
         return leaderBoardService.getCurrentLeaderBoard();
     }
 }
